c17fc194c1242ef5cffc8a41dff7200467aea7ce
John Kenneth Romero <jeromero@csmbataan.ph.education>
2020-10-26 18:10:18 +0400

    CREATED FILE FOR FIRST QUARTERLY ASSESSMENT

---
+++ FIRST QUARTERLY ASSESSMENTY/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FIRST_QUARTERLY_ASSESSMENTY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
